fd922e3e103a68dc799bfe280f98afd39fdadc57
niranjan112 <niranjan112>
2019-09-14 14:42:27 +0530

    Second commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="grey--text">
+                <span class="font-weight-light">TODO</span>
+                <span>NINJA</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn text color="grey">
+                <span>Exit</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer app v-model="drawer" class="grey lighten-3" temporary>
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route" link>
+                    <v-list-item-icon>
+                        <v-icon color="grey">{{ link.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title class="font-weight-light">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+            links: [
+                {icon: 'dashboard', text: 'Dashboard', route: '/'},
+                {icon: 'folder', text: 'My Projects', route: '/projects'},
+                {icon: 'person', text: 'Team', route: '/team'},
+            ]
+        }
+    }
+}
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+    <h1 class="grey--text">Dashboard</h1>
+
+    <v-container class="my-5 text-justify">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore quo eos alias illum quasi saepe incidunt repellendus voluptates nesciunt, amet eum quod tenetur, nobis error iste deserunt pariatur! Dolores, quaerat.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, natus vel. Repellendus accusantium doloribus beatae impedit! Fugiat quaerat fugit, dicta perspiciatis saepe delectus possimus id ea quasi deserunt illo praesentium!</p>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
